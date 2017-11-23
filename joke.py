0c13ed1ac182683a20f663e334097ebb2ea6c345
Humza S <humzas@berkeley.edu>
2017-11-22 23:33:50 -0800

    added background picture, and drop down menu to select filter for jokes

--- joke.py
+++ joke.py
@@ -1,27 +1,25 @@
 import praw
-import config
-import random
-from datetime import datetime
+import config as c
+
 
 class Joke_Gen:
 	def __init__(self, sub):
-		self.sub = sub
-		self.r = praw.Reddit(username = config.username,
-			password = config.password,
-			client_id = config.client_id,
-			client_secret = config.client_secret,
-			user_agent = "humza's joke scraper")
-	
-	def get_jokes(self):
-		self.jokes = {submission.title : submission.selftext for 
-			submission in self.r.subreddit(self.sub).top(limit=200)
-			if (len(submission.selftext) < 1000) 
-			and "edit".lower() not in 
-			submission.selftext.lower()}
+		r = praw.Reddit(username = c.username,
+				 password = c.password,
+				 client_id = c.client_id,
+				 client_secret = c.client_secret,
+				 user_agent = "humza's joke scraper")
+		self.jokes = [submission for
+			submission in r.subreddit(sub).top(limit=10)
+			if (len(submission.selftext) < 1000)
+			and "edit".lower() not in
+			submission.selftext.lower()]
 
-	def get_joke(self):
-		rand = random.randint(0, len(self.jokes) - 1)
-		title = list(self.jokes.keys())[rand]
-		text = list(self.jokes.values())[rand]
-		
+	def get_all(self, index):
+		title = self.jokes[index].title
+		text = self.jokes[index].selftext
 		return title, text
+	
+	def get_submission(self, index):
+		submission = self.jokes[index]
+		return submission
--- window.py
+++ window.py
@@ -1,44 +1,83 @@
 import sys
 import joke as j
-import textwrap
-from PyQt5 import QtWidgets
+import random
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import QRect
 
 jg = j.Joke_Gen('jokes')
-jg.get_jokes()
-
-class Window(QtWidgets.QWidget):
+class Window(QWidget):
 
     def __init__(self):
         super().__init__()
-        self.init_ui()
+        self.setup()
 
-    def init_ui(self):
-        self.b = QtWidgets.QPushButton('Get a Joke')
-        self.l = QtWidgets.QLabel('')
-
-        h_box = QtWidgets.QHBoxLayout()
-        h_box.addWidget(self.l)
-
-        v_box = QtWidgets.QVBoxLayout()
-        v_box.addWidget(self.b)
-        v_box.addLayout(h_box)
-
-        self.setGeometry(400, 200, 600, 500)
-        self.setLayout(v_box)
-        self.setWindowTitle('Joke Generator')
-        self.show()
-        self.b.clicked.connect(self.btn_click)
+    def setup(self):
+        self.pic = QLabel('', self)
+        self.set_picture(self.pic)
+        self.b = QPushButton('New Joke', self)
+        self.b.move(10, 10)
+        self.l = QLabel('', self)
+        self.l.move(20, 50)
         self.l.setWordWrap(True)
+        self.l.setGeometry(QRect(70, 80, 500, 400))
+        self.setGeometry(400, 200, 800, 550)
+        self.setWindowTitle('Joke Generator')
+        self.cBox = QComboBox(self)
+        self.cBox.move(130, 12)
+        
+        self.b.clicked.connect(self.new_click)
+        self.show()
+        # self.show()
+        # radiobutton = QRadioButton("Brazil")
+        # radiobutton.setChecked(True)
+        # radiobutton.country = "Brazil"
+        # radiobutton.toggled.connect(self.on_radio_button_toggled)
+        # layout.addWidget(radiobutton, 0, 0)
 
-    def btn_click(self):
-        title, old_text = jg.get_joke()
+        # radiobutton = QRadioButton("Argentina")
+        # radiobutton.country = "Argentina"
+        # radiobutton.toggled.connect(self.on_radio_button_toggled)
+        # layout.addWidget(radiobutton, 0, 1)
+
+        # radiobutton = QRadioButton("Ecuador")
+        # radiobutton.country = "Ecuador"
+        # radiobutton.toggled.connect(self.on_radio_button_toggled)
+        # layout.addWidget(radiobutton, 0, 2)
+
+    # def on_radio_button_toggled(self):
+    #     radiobutton = self.sender()
+
+    #     if radiobutton.isChecked():
+    #         print("Selected country is %s" % (radiobutton.country))
+
+    #def init_ui(self):
+        # h_box = q.QHBoxLayout()
+        # h_box.addWidget(self.l)
+        # h_box.addWidget(self.pic)
+
+        # v_box = q.QVBoxLayout()
+        # v_box.addWidget(self.b1)
+        # v_box.addLayout(h_box)
+
+        
+        # self.setLayout(v_box)
+        
+        #self.set_picture()
+        
+    def set_picture(self, pic):
+        pixmap = QPixmap('dadjoke.png')
+        pic.setPixmap(pixmap)
+
+    def new_click(self):
+        rand = random.randint(0, len(jg.jokes) - 1)
+        title, old_text = jg.get_all(rand)
         self.l.setText(title
-        + "\n---------------------------------------------" + 
-        "-------------------------------------------------"
-         + "\n\n" + old_text)
+        # + "\n---------------------------------------------" + 
+        # "-------------------------------------------------"
+        + "\n\n" + old_text)
 
-
-app = QtWidgets.QApplication(sys.argv)
+app = QApplication(sys.argv)
 a_window = Window()
 app.exec_()
 
